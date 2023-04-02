8a9e7000b0a1fd46f2345b83618ae88af270591c
Alibordi <alibordi.muhammad@cern.ch>
2023-04-02 16:24:59 +0200

    blank analyzers

---
+++ ScaleFactor/plugins/ScaleFactor.cc
@@ -0,0 +1,136 @@
+// -*- C++ -*-
+//
+// Package:    Leptonic_FinalState/ScaleFactor
+// Class:      ScaleFactor
+//
+/**\class ScaleFactor ScaleFactor.cc Leptonic_FinalState/ScaleFactor/plugins/ScaleFactor.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Alibordi Muhammad
+//         Created:  Sun, 02 Apr 2023 14:23:24 GMT
+//
+//
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<>
+// This will improve performance in multithreaded jobs.
+
+using reco::TrackCollection;
+
+class ScaleFactor : public edm::one::EDAnalyzer<edm::one::SharedResources> {
+public:
+  explicit ScaleFactor(const edm::ParameterSet&);
+  ~ScaleFactor() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+  void beginJob() override;
+  void analyze(const edm::Event&, const edm::EventSetup&) override;
+  void endJob() override;
+
+  // ----------member data ---------------------------
+  edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+  edm::ESGetToken<SetupData, SetupRecord> setupToken_;
+#endif
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+ScaleFactor::ScaleFactor(const edm::ParameterSet& iConfig)
+    : tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks"))) {
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+  setupDataToken_ = esConsumes<SetupData, SetupRecord>();
+#endif
+  //now do what ever initialization is needed
+}
+
+ScaleFactor::~ScaleFactor() {
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
+  //
+  // please remove this method altogether if it would be left empty
+}
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void ScaleFactor::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  using namespace edm;
+
+  for (const auto& track : iEvent.get(tracksToken_)) {
+    // do something with track parameters, e.g, plot the charge.
+    // int charge = track.charge();
+  }
+
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+  // if the SetupData is always needed
+  auto setup = iSetup.getData(setupToken_);
+  // if need the ESHandle to check if the SetupData was there or not
+  auto pSetup = iSetup.getHandle(setupToken_);
+#endif
+}
+
+// ------------ method called once each job just before starting event loop  ------------
+void ScaleFactor::beginJob() {
+  // please remove this method if not needed
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void ScaleFactor::endJob() {
+  // please remove this method if not needed
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void ScaleFactor::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+
+  //Specify that only 'tracks' is allowed
+  //To use, remove the default given above and uncomment below
+  //ParameterSetDescription desc;
+  //desc.addUntracked<edm::InputTag>("tracks","ctfWithMaterialTracks");
+  //descriptions.addWithDefaultLabel(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(ScaleFactor);
