1ab14c64a79081e73b26848eb2acb30abe6a10a4
Alibordi <alibordi.muhammad@cern.ch>
2023-07-05 14:58:59 +0200

    producer created

---
+++ CalibProducer/plugins/CalibProducer.cc
@@ -0,0 +1,162 @@
+// -*- C++ -*-
+//
+// Package:    Leptonic_FinalState/CalibProducer
+// Class:      CalibProducer
+//
+/**\class CalibProducer CalibProducer.cc Leptonic_FinalState/CalibProducer/plugins/CalibProducer.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Alibordi Muhammad
+//         Created:  Wed, 05 Jul 2023 12:58:01 GMT
+//
+//
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/StreamID.h"
+
+//
+// class declaration
+//
+
+class CalibProducer : public edm::stream::EDProducer<> {
+public:
+  explicit CalibProducer(const edm::ParameterSet&);
+  ~CalibProducer() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+  void beginStream(edm::StreamID) override;
+  void produce(edm::Event&, const edm::EventSetup&) override;
+  void endStream() override;
+
+  //void beginRun(edm::Run const&, edm::EventSetup const&) override;
+  //void endRun(edm::Run const&, edm::EventSetup const&) override;
+  //void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+  //void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+
+  // ----------member data ---------------------------
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+CalibProducer::CalibProducer(const edm::ParameterSet& iConfig) {
+  //register your products
+  /* Examples
+  produces<ExampleData2>();
+
+  //if do put with a label
+  produces<ExampleData2>("label");
+ 
+  //if you want to put into the Run
+  produces<ExampleData2,InRun>();
+  */
+  //now do what ever other initialization is needed
+}
+
+CalibProducer::~CalibProducer() {
+  // do anything here that needs to be done at destruction time
+  // (e.g. close files, deallocate resources etc.)
+  //
+  // please remove this method altogether if it would be left empty
+}
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+void CalibProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  using namespace edm;
+  /* This is an event example
+  //Read 'ExampleData' from the Event
+  ExampleData const& in = iEvent.get(inToken_);
+
+  //Use the ExampleData to create an ExampleData2 which 
+  // is put into the Event
+  iEvent.put(std::make_unique<ExampleData2>(in));
+  */
+
+  /* this is an EventSetup example
+  //Read SetupData from the SetupRecord in the EventSetup
+  SetupData& setup = iSetup.getData(setupToken_);
+  */
+}
+
+// ------------ method called once each stream before processing any runs, lumis or events  ------------
+void CalibProducer::beginStream(edm::StreamID) {
+  // please remove this method if not needed
+}
+
+// ------------ method called once each stream after processing all runs, lumis and events  ------------
+void CalibProducer::endStream() {
+  // please remove this method if not needed
+}
+
+// ------------ method called when starting to processes a run  ------------
+/*
+void
+CalibProducer::beginRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when ending the processing of a run  ------------
+/*
+void
+CalibProducer::endRun(edm::Run const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when starting to processes a luminosity block  ------------
+/*
+void
+CalibProducer::beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+/*
+void
+CalibProducer::endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&)
+{
+}
+*/
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void CalibProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(CalibProducer);
